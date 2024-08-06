9e4f6dc58217d381aaf8df2eba7f7fa4a6fd880a
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-08-06 15:58:52 -0500

    add nav link scroll and inview behavior

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,19 +1,40 @@
-"use client";
-
 import React from "react";
 import Hero from "./hero";
 import { useParams, useSearchParams } from "next/navigation";
 
-export default function Header() {
-  const params = useSearchParams();
-  console.log("params", params);
+type headerProps = {
+  links: string[];
+  activeSection: string;
+};
+
+export default function Header({ links, activeSection }: headerProps) {
   return (
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
       <div>
         <Hero />
         <nav className="hidden lg:block">
           <ul className="mt-16 w-max">
-            <li>
+            {links.map((link) => (
+              <li key={link}>
+                <a href={`#${link}`} className="group flex items-center py-3">
+                  <span
+                    className={`w-10 bg-accent h-[2px] mr-3 group-hover:w-16 transition-all ${
+                      activeSection === link && "w-16"
+                    }`}
+                  ></span>
+                  <span
+                    className={`uppercase tracking-widest group-hover:text-lightText text-sm ${
+                      activeSection === link
+                        ? "text-lightText"
+                        : "text-secondaryText"
+                    }`}
+                  >
+                    {link}
+                  </span>
+                </a>
+              </li>
+            ))}
+            {/* <li>
               <a href="#about" className="group flex items-center py-3">
                 <span className="w-10 bg-accent h-[2px] mr-3 group-hover:w-16 transition-all"></span>
                 <span className="uppercase tracking-widest text-secondaryText group-hover:text-lightText text-sm">
@@ -36,7 +57,7 @@ export default function Header() {
                   projects
                 </span>
               </a>
-            </li>
+            </li> */}
           </ul>
         </nav>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,81 @@
+"use client";
+
 import { motion } from "framer-motion";
 import Hero from "./components/hero";
 import About from "./components/about";
 import Header from "./components/header";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  let [activeSection, setActiveSection] = useState("about");
+
+  let links = ["about", "experience", "projects"];
+
+  useEffect(() => {
+    let about = document.getElementById("about");
+    let projects = document.getElementById("experience");
+    let skills = document.getElementById("projects");
+
+    let sections = [about, projects, skills];
+
+    const observerOptions = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          if (entry.target.id == "about") {
+            setActiveSection("about");
+          }
+          if (entry.target.id == "experience") {
+            setActiveSection("experience");
+          }
+          if (entry.target.id == "projects") {
+            setActiveSection("projects");
+          }
+        }
+      });
+    }, observerOptions);
+
+    sections.forEach((section) => {
+      section && observer.observe(section);
+    });
+  }, []);
+
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0 bg-slate-800">
       <div className="lg:flex lg:justify-between lg:gap-4">
-        <Header />
+        <Header links={links} activeSection={activeSection} />
         <main className="pt-24 lg:w-1/2 lg:py-24">
+          {/* about */}
           <section
             id="about"
-            className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+            className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 "
           >
             <About />
           </section>
+
+          {/* experience */}
           <section
             id="experience"
             className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
           >
             Experience
           </section>
+
+          {/* projects */}
           <section
             id="projects"
-            className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+            className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24]"
           >
             Projects
           </section>
-          <section className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
+
+          {/* additional stuff */}
+          <section className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 ">
             Tech Stack
           </section>
           <section className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
