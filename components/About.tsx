2ea837a851c8b0879715d27882295a4e1bc3e3ec
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-08-11 15:13:33 -0500

    update about me text and styling

--- components/About.tsx
+++ components/About.tsx
@@ -8,34 +8,29 @@ function About() {
           About
         </h2>
       </div>
-      <div className="leading-7 text-lightText">
+      <div className="font-light leading-7 text-lightText">
         <p className="mb-4">
-          Hi! My name is Eddie and I&apos;m a software engineer based in Austin,
-          Texas with a specialty in the JavaScript/TypeScript ecosystem. I enjoy
-          developing pixel perfect solutions that focus heavily on delivering an
-          intuitive user experience.
+          A software engineer based in Austin, Texas with a specialty in the
+          JavaScript/TypeScript ecosystem. I enjoy developing pixel-perfect
+          solutions that emphasize on delivering an intuitive user experience.
         </p>
         <p className="mb-4">
-          My inspiration in what made me pursue this field is accredited towards
-          Apple. I still remember the twelve year old me getting my hands on my
-          first iPhone and navigating through the endless features that my mind
-          couldn&apos;t even fully comprehend at the time.
+          My inspiration to pursue this field is accredited towards Apple. I
+          vividly remember the twelve year old me getting my hands on my first
+          iPhone and navigating through the endless features that sparked a
+          fascination in technology that has stayed with me ever since. Today, I
+          strive to bring the same experiences to my users. My main focus in
+          developing software is to create an exquisite user experience while
+          bringing applications to life with aesthetic user interfaces.
         </p>
+        <p className="mb-4"></p>
         <p className="mb-4">
-          Fast forward to today, I still chase a form of that feeling. Instead
-          of chasing the feeling for myself, that feeling has become a core
-          principle I strive to bring to my users. Aside from developing
-          aesthetic user interfaces, my goal as the developer has been to create
-          an exquisite user experience while solving problems that the average
-          consumer face through software.
-        </p>
-        <p className="mb-4">
-          While I&apos;m not at my computer, you can typically find me training
-          at the gym, bouldering, hiking with my two dogs (Corgi and Shiba Inu),
-          or just casually lounging with my S.O watching some popular shows. If
-          you see something you like here or would be interested in
-          collaborating on something, my door is always open to chat about tech,
-          fitness, or life!✌️
+          While I&apos;m not at my computer, you can typically find me working
+          out, bouldering, hiking with my two dogs (Corgi and Shiba Inu), or
+          just casually lounging with my S.O watching some popular shows. If you
+          see something you like or would be interested in collaborating on
+          something, my door is always open to chat about tech, fitness, or
+          life!
         </p>
       </div>
     </>
