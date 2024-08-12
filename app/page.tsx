6517b1dbb809e9edb293bdcc0752eb4849a8d786
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-08-12 16:53:39 -0500

    change bg color

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 
 function Home() {
   return (
-    <div className="mx-auto min-h-screen max-w-screen-xl bg-slate-800 px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
+    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
       <div className="lg:flex lg:justify-between lg:gap-4">
         <Header />
         <main className="pt-24 lg:w-1/2 lg:py-24">
