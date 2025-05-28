c2e9b4d44907b1baafa890181691d23eeb0a5d05
Eddie-Kuo <Eddiekuo97@gmail.com>
2025-05-28 17:43:05 -0500

    update packages, experience, and resume

--- constants/experience.ts
+++ constants/experience.ts
@@ -2,10 +2,19 @@ export const experiences = [
   {
     id: 1,
     title: "Software Engineer -",
+    location: "Digital Uprising LLC",
+    description:
+      "Led the development of an internal platform that transformed how the team managed accounts and messaging—streamlining workflows and eliminating longstanding bottlenecks. Worked closely with stakeholders to bring ideas to life and deliver a tool the entire company now relies on.",
+    duration: "2024 - Present",
+    technologies: ["TypeScript", "React", "Next.js", "Nest.js", "React Native"],
+  },
+  {
+    id: 2,
+    title: "Software Engineer -",
     location: "Formosa Plastics Corporation",
     description:
       "Redesigned and developed the department's intranet site, migrating it from Joomla to React with other modern technologies that ultimately elevated the user experience across departments. Closely worked with digitization team to implement a mobile safety accountability app used by technicians when entering powerplants in case of emergency.",
-    duration: "2024 - PRESENT",
+    duration: "June - Dec. 2024",
     technologies: [
       "JavaScript",
       "TypeScript",
@@ -17,29 +26,12 @@ export const experiences = [
     ],
   },
   {
-    id: 2,
+    id: 3,
     title: "Software Engineer -",
     location: "Freelance",
     description:
       "Designed and developed high-quality websites for local small businesses, helping them boost their internet presence. Assisted businesses by developing internal tools to streamline their workflow by providing solutions that automated recurring tasks.",
     duration: "2022 - 2023",
     technologies: ["TypeScript", "Next.js", "React", "Figma", "Tailwind CSS"],
-  },
-  {
-    id: 3,
-    title: "Software Engineer -",
-    location: "Alchemy Code Lab",
-    description:
-      "Coding bootcamp where I learned the ins and outs of fullstack web development. Over the course of six months, I developed and deployed over 75 applications from scratch, mastering the fundamentals along with advanced techniques of web development.",
-    duration: "July - Dec 2022",
-    technologies: [
-      "JavaScript",
-      "HTML & CSS",
-      "React",
-      "Node.js",
-      "Express",
-      "SQL",
-      "React Native",
-    ],
   },
 ];
